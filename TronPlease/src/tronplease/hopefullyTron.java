8eedc3c96d07560a97dacc157795feaa7d77e8d4
Benjamin Wong <benjamin.wong@brentwood.ca>
2017-05-15 17:35:42 -0700

    Added a grid class 	Draws a grid on the screen (can specify dimensions of said grid)

--- TronPlease/src/tronplease/hopefullyTron.java
+++ TronPlease/src/tronplease/hopefullyTron.java
@@ -6,7 +6,9 @@
 package tronplease;
 
 import environment.Environment;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
@@ -19,28 +21,36 @@ class hopefullyTron extends Environment {
     public hopefullyTron() {
     }
 
+    //<editor-fold defaultstate="collapsed" desc="Abstract Methods">
     @Override
     public void initializeEnvironment() {
+        grid = new Grid(100, 50, 10, new Point(0, 0), Color.BLACK);
     }
-
+    
     @Override
     public void timerTaskHandler() {
     }
-
+    
     @Override
     public void keyPressedHandler(KeyEvent e) {
     }
-
+    
     @Override
     public void keyReleasedHandler(KeyEvent e) {
     }
-
+    
     @Override
     public void environmentMouseClicked(MouseEvent e) {
     }
-
+    
     @Override
     public void paintEnvironment(Graphics graphics) {
+        if (grid != null) {
+            grid.drawGrid(graphics);
+        }
     }
+//</editor-fold>
+    
+    private Grid grid;
     
 }
